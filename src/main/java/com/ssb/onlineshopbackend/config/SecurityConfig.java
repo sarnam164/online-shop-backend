cf358c1b729442fcd817363480ffc68970096487
Gohan <sarnam164@gmail.com>
2021-09-27 11:54:49 +0530

    Enabling cors

--- src/main/java/com/ssb/onlineshopbackend/config/SecurityConfig.java
+++ src/main/java/com/ssb/onlineshopbackend/config/SecurityConfig.java
@@ -35,8 +35,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors().and()
                 .csrf().disable()
-                .cors().disable()
                 .authorizeRequests()
                 .antMatchers("/token","/welcome").permitAll()
                 .anyRequest().authenticated()
